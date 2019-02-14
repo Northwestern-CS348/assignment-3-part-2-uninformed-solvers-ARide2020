ad7f678749057ff4936606dbc20931eb2e5b039c
ARide2020 <44145055+ARide2020@users.noreply.github.com>
2019-02-13 21:30:58 -0600

    Update student_code_game_masters.py

--- student_code_game_masters.py
+++ student_code_game_masters.py
@@ -1,6 +1,7 @@
 from game_master import GameMaster
 from read import *
 from util import *
+import pdb
 
 class TowerOfHanoiGame(GameMaster):
 
@@ -34,7 +35,85 @@ class TowerOfHanoiGame(GameMaster):
             A Tuple of Tuples that represent the game state
         """
         ### student code goes here
-        pass
+
+        don = [False,False,False,False,False]
+
+        askp1 = parse_input("fact: (empty peg1)")
+        askp2 = parse_input("fact: (empty peg2)")
+        askp3 = parse_input("fact: (empty peg3)")
+
+        Peg1 = []
+        Peg2 = []
+        Peg3 = []
+
+        pegs1 = []
+        pegs2 = []
+        pegs3 = []
+        i = 1
+        
+        while i < 6:
+            pegs1.append(parse_input("fact: (on disk" + str(i) + " peg1)"))
+            pegs2.append(parse_input("fact: (on disk" + str(i) + " peg2)"))
+            pegs3.append(parse_input("fact: (on disk" + str(i) + " peg3)"))
+            i = i + 1
+        
+
+        if self.kb.kb_ask(askp1) != False:
+            Peg1 = ()
+        else:
+            cf = 0
+            cb = 1
+            
+            for f in pegs1:
+                if self.kb.kb_ask(f) != False:
+                    don[cf] = True
+                cf = cf + 1
+            for b in don:
+                if b == True:
+                    Peg1.append(cb)
+                cb = cb + 1
+            Peg1 = tuple(Peg1)
+            don = [False,False,False,False,False]
+
+
+        if self.kb.kb_ask(askp2) != False:
+            Peg2 = ()
+        else:
+            cf = 0
+            cb = 1
+            
+            for f in pegs2:
+                if self.kb.kb_ask(f) != False:
+                    don[cf] = True
+                cf = cf + 1
+            for b in don:
+                if b == True:
+                    Peg2.append(cb)
+                cb = cb + 1
+            Peg2 = tuple(Peg2)
+            don = [False,False,False,False,False]
+
+        if self.kb.kb_ask(askp3) != False:
+           Peg3 = ()
+        else:
+            cf = 0
+            cb = 1
+            
+            for f in pegs3:
+                if self.kb.kb_ask(f) != False:
+                    don[cf] = True
+                cf = cf + 1
+            for b in don:
+                if b == True:
+                    Peg3.append(cb)
+                cb = cb + 1
+            Peg3 = tuple(Peg3)
+            don = [False,False,False,False,False]
+
+        theTups = (Peg1 , Peg2 , Peg3)
+        return theTups
+
+
 
     def makeMove(self, movable_statement):
         """
@@ -53,7 +132,100 @@ class TowerOfHanoiGame(GameMaster):
             None
         """
         ### Student code goes here
-        pass
+        #basically just adding facts and retracting facts
+        theDisk = movable_statement.terms[0].term.element
+        frompeg = movable_statement.terms[1].term.element
+        topeg = movable_statement.terms[2].term.element
+
+        newlocation = parse_input("fact: (on " + theDisk + " " + topeg + ")")
+        oldlocation = parse_input("fact: (on " + theDisk + " " + frompeg + ")")
+         
+        self.kb.kb_assert(newlocation)
+        self.kb.kb_retract(oldlocation)
+
+        belowDisk = [False,False,False,False,False]
+        askBelow = []
+        i = 1
+        while i < 6:
+            askBelow.append(parse_input("fact: (ont " + theDisk + " disk" + str(i) + ")"))
+            i = i + 1
+
+        cf = 0
+        cb = 1
+
+        for f in askBelow:
+            if self.kb.kb_ask(f) != False:
+                belowDisk[cf] = True
+                break
+            cf = cf + 1
+
+        for b in belowDisk:
+            if b == True:
+                break
+            cb = cb + 1
+
+        #making sure theere is something under disk were moving if we need to make changes or nah
+        if cb != 6:
+            fromPegBelowDisk = "disk" + str(cb)
+
+            newTopFromPeg = parse_input("fact: (TheTop " + fromPegBelowDisk + " " + frompeg + ")")
+            self.kb.kb_assert(newTopFromPeg)
+
+            oldOnt = parse_input("fact: (ont " + theDisk + " " + fromPegBelowDisk + ")")        
+            self.kb.kb_retract(oldOnt)
+        else:
+            #since there was nothing under disk this peg is now empty
+            FromPegIsEmpty = parse_input("fact: (empty " + frompeg + ")")
+            self.kb.kb_assert(FromPegIsEmpty)
+
+        oldTopFromPeg = parse_input("fact: (TheTop " + theDisk + " " + frompeg + ")")
+        self.kb.kb_retract(oldTopFromPeg)
+
+        
+
+
+        topegEmpty = False
+        checkEmpty = parse_input("fact: (empty + " + topeg + ")")
+
+        if self.kb.kb_ask(checkEmpty) != False:
+            topegEmpty = True
+
+        if topegEmpty == False:
+            toPegTop = [False,False,False,False,False]
+            askTop = []
+            i = 1
+            while i < 6:
+                askTop.append(parse_input("fact: (TheTop disk" + str(i) + " " + topeg + ")"))
+                i = i + 1
+
+            cf = 0
+            cb = 1
+
+            for f in askBelow:
+                if self.kb.kb_ask(f) != False:
+                    toPegTop[cf] = True
+                    break
+                cf = cf + 1
+
+            for b in askTop:
+                if b == True:
+                    break
+                cb = cb + 1
+
+            toPegTopDisk = "disk" + str(cb)
+            oldTopToPeg = parse_input("fact: (TheTop " + toPegTopDisk + " " + topeg + ")")
+            self.kb.kb_retract(oldTopToPeg)
+
+            newOnt = parse_input("fact: (ont " + theDisk + " " + toPegTopDisk + ")")
+            self.kb.kb_assert(newOnt)
+
+        newTopToPeg = parse_input("fact: (TheTop " + theDisk + " " + topeg + ")")
+        self.kb.kb_assert(newTopToPeg)
+
+        #since it was empty before we have to say it no longer is
+        toPegWasEmpty = parse_input("fact: (empty " + topeg + ")")
+        self.kb.kb_retract(toPegWasEmpty)
+      
 
     def reverseMove(self, movable_statement):
         """
@@ -100,7 +272,211 @@ class Puzzle8Game(GameMaster):
             A Tuple of Tuples that represent the game state
         """
         ### Student code goes here
-        pass
+        i = 1
+        j = 1
+        t1facts = []
+        t2facts = []
+        t3facts = []
+        t4facts = []
+        t5facts = []
+        t6facts = []
+        t7facts = []
+        t8facts = []
+        emfacts = []
+
+        firstRow = []
+        secondRow = []
+        thirdRow = []
+
+        for i in range(1,4):
+            for j in range(1,4):
+                t1facts.append(parse_input("fact: (position tile1 pos" + str(i) + " pos" + str(j) + ")"))
+                t2facts.append(parse_input("fact: (position tile2 pos" + str(i) + " pos" + str(j) + ")"))
+                t3facts.append(parse_input("fact: (position tile3 pos" + str(i) + " pos" + str(j) + ")"))
+                t4facts.append(parse_input("fact: (position tile4 pos" + str(i) + " pos" + str(j) + ")"))
+                t5facts.append(parse_input("fact: (position tile5 pos" + str(i) + " pos" + str(j) + ")"))
+                t6facts.append(parse_input("fact: (position tile6 pos" + str(i) + " pos" + str(j) + ")"))
+                t7facts.append(parse_input("fact: (position tile7 pos" + str(i) + " pos" + str(j) + ")"))
+                t8facts.append(parse_input("fact: (position tile8 pos" + str(i) + " pos" + str(j) + ")"))
+                emfacts.append(parse_input("fact: (position empty pos" + str(i) + " pos" + str(j) + ")"))
+            
+        
+        #what mean [(1,1),(2,1),(3,1),(1,2),(2,2),(3,2),(1,3),(2,3),(3,3)]
+        cf = 0
+        t1 = []
+        for f in t1facts:
+            if self.kb.kb_ask(f) != False:
+                break
+            cf = cf + 1
+        if(cf == 0 or cf == 3 or cf == 6):
+            t1 = [1,cf]
+            firstRow.append(t1)
+        elif(cf == 1 or cf == 4 or cf == 7):
+            t1 = [1,cf]
+            secondRow.append(t1)
+        else:
+            t1 = [1,cf]
+            thirdRow.append(t1)
+
+        cf = 0
+        t2 = []
+        for f in t2facts:
+            if self.kb.kb_ask(f) != False:
+                break
+            cf = cf + 1
+        if(cf == 0 or cf == 3 or cf == 6):
+            t2 = [2,cf]
+            firstRow.append(t2)
+        elif(cf == 1 or cf == 4 or cf == 7):
+            t2 = [2,cf]
+            secondRow.append(t2)
+        else:
+            t2 = [2, cf]
+            thirdRow.append(t2)
+
+        cf = 0
+        t3 = []
+        for f in t3facts:
+            if self.kb.kb_ask(f) != False:
+                break
+            cf = cf + 1
+        if(cf == 0 or cf == 3 or cf == 6):
+            t3 = [3,cf]
+            firstRow.append(t3)
+        elif(cf == 1 or cf == 4 or cf == 7):
+            t3 = [3,cf]
+            secondRow.append(t3)
+        else:
+            t3 = [3, cf]
+            thirdRow.append(t3)
+
+        cf = 0
+        t4 = []
+        for f in t4facts:
+            if self.kb.kb_ask(f) != False:
+                break
+            cf = cf + 1
+        if(cf == 0 or cf == 3 or cf == 6):
+            t4 = [4,cf]
+            firstRow.append(t4)
+        elif(cf == 1 or cf == 4 or cf == 7):
+            t4 = [4,cf]
+            secondRow.append(t4)
+        else:
+            t4 = [4,cf]
+            thirdRow.append(t4)
+
+        cf = 0
+        t5 = []
+        for f in t5facts:
+            if self.kb.kb_ask(f) != False:
+                break
+            cf = cf + 1
+        if(cf == 0 or cf == 3 or cf == 6):
+            t5 = [5,cf]
+            firstRow.append(t5)
+        elif(cf == 1 or cf == 4 or cf == 7):
+            t5 = [5,cf]
+            secondRow.append(t5)
+        else:
+            t5 = [5, cf]
+            thirdRow.append(t5)
+
+        cf = 0
+        t6 = []
+        for f in t6facts:
+            if self.kb.kb_ask(f) != False:
+                break
+            cf = cf + 1
+        if(cf == 0 or cf == 3 or cf == 6):
+            t6 = [6,cf]
+            firstRow.append(t6)
+        elif(cf == 1 or cf == 4 or cf == 7):
+            t6 = [6,cf]
+            secondRow.append(t6)
+        else:
+            t6 = [6, cf]
+            thirdRow.append(t6)
+        
+        cf = 0
+        t7 = []
+        for f in t7facts:
+            if self.kb.kb_ask(f) != False:
+                break
+            cf = cf + 1
+        if(cf == 0 or cf == 3 or cf == 6):
+            t7 = [7,cf]
+            firstRow.append(t7)
+        elif(cf == 1 or cf == 4 or cf == 7):
+            t7 = [7,cf]
+            secondRow.append(t7)
+        else:
+            t7 = [7,cf]
+            thirdRow.append(t7)
+
+        cf = 0
+        t8 = []
+        for f in t8facts:
+            if self.kb.kb_ask(f) != False:
+                break
+            cf = cf + 1
+        if(cf == 0 or cf == 3 or cf == 6):
+            t8 = [8,cf]
+            firstRow.append(t8)
+        elif(cf == 1 or cf == 4 or cf == 7):
+            t8 = [8,cf]
+            secondRow.append(t8)
+        else:
+            t8 = [8, cf]
+            thirdRow.append(t8)
+
+        cf = 0
+        em = []
+        for f in emfacts:
+            if self.kb.kb_ask(f) != False:
+                break
+            cf = cf + 1
+        if(cf == 0 or cf == 3 or cf == 6):
+            em = [-1,cf]
+            firstRow.append(em)
+        elif(cf == 1 or cf == 4 or cf == 7):
+            em = [-1,cf]
+            secondRow.append(em)
+        else:
+            em = [-1, cf]
+            thirdRow.append(em)
+
+        for i in range(0,2):
+            for j in range(1,3):
+                if firstRow[i][1] > firstRow[j][1]:
+                    firstRow[j], firstRow[i] = firstRow[i], firstRow[j]
+        
+        for i in range(0,2):
+            for j in range(1,3):
+                if secondRow[i][1] > secondRow[j][1]:
+                    secondRow[j], secondRow[i] = secondRow[i], secondRow[j]
+
+        for i in range(0,2):
+            for j in range(1,3):
+                if thirdRow[i][1] > thirdRow[j][1]:
+                    thirdRow[j], thirdRow[i] = thirdRow[i], thirdRow[j]
+        
+        onerow = []
+        tworow = []
+        threerow = []
+
+        for i in range(0,3):
+            onerow.append(firstRow[i][0])
+            tworow.append(secondRow[i][0])
+            threerow.append(thirdRow[i][0])
+        
+        firstRow = tuple(onerow)
+        secondRow = tuple(tworow)
+        thirdRow = tuple(threerow)
+
+        theTups = (firstRow, secondRow, thirdRow)
+        return theTups
+
 
     def makeMove(self, movable_statement):
         """
@@ -119,7 +495,24 @@ class Puzzle8Game(GameMaster):
             None
         """
         ### Student code goes here
-        pass
+        theTile = movable_statement.terms[0].term.element
+        fromPosX = movable_statement.terms[1].term.element
+        fromPosY = movable_statement.terms[2].term.element
+        toPosX = movable_statement.terms[3].term.element
+        toPosY = movable_statement.terms[4].term.element
+        
+        NewTileLoc = parse_input("fact: (position " + theTile + " " + toPosX + " " + toPosY + ")")
+        NewEmptyLoc = parse_input("fact: (position empty " + fromPosX + " " + fromPosY + ")")
+
+        OldTileLoc = parse_input("fact: (position " + theTile + " " + fromPosX + " " + fromPosY + ")")
+        OldEmptyLoc = parse_input("fact: (position empty " + toPosX + " " + toPosY + ")")
+
+        self.kb.kb_assert(NewTileLoc)
+        self.kb.kb_assert(NewEmptyLoc)
+
+        self.kb.kb_retract(OldEmptyLoc)
+        self.kb.kb_retract(OldTileLoc)
+        
 
     def reverseMove(self, movable_statement):
         """
