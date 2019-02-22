8e8e27a3b0478648f6a33f1b713500299836e49e
ARide2020 <44145055+ARide2020@users.noreply.github.com>
2019-02-21 21:25:24 -0600

    Update student_code_uninformed_solvers.py

--- student_code_uninformed_solvers.py
+++ student_code_uninformed_solvers.py
@@ -1,5 +1,6 @@
 
 from solver import *
+import queue
 
 class SolverDFS(UninformedSolver):
     def __init__(self, gameMaster, victoryCondition):
@@ -27,61 +28,75 @@ class SolverDFS(UninformedSolver):
         
         theGame = self.gm #just the game master not game state
         victory = self.victoryCondition
-        checkState = self.currentState.state
-        visted = self.visited[self.currentState]
+        checkState = theGame.getGameState()
+        #visted = self.visited[self.currentState]
 
-        if checkState == victory:
+        if(checkState == victory):
             return True ##or do nothing
 
-        if(self.visited[self.currentState] == False):
-            self.visited[self.currentState] = True
+        dep = self.currentState.depth
+        childrenLen = len(self.gm.getMovables())
+        childs = self.gm.getMovables()
 
-            dep = self.currentState.depth
-            childrenLen = len(self.gm.getMovables())
-            childs = self.gm.getMovables()
+        if(childrenLen != 0):#if theres no kids were at the bottom
 
-            if childrenLen != 0:
-                for i in range(0,childrenLen):
-                    nextState = childs[i]
-                    theGame.makeMove(nextState)
-                    self.currentState.children.append(GameState(theGame.getGameState(), dep + 1, nextState))
-                    theGame.reverseMove(nextState)
-                    self.currentState.children[i].parent = self.currentState
+            for i in range(0,childrenLen):#just putting all the kids with their parent
+                nextMove = childs[i]
+                theGame.makeMove(nextMove)
 
+                childState = GameState(theGame.getGameState(), dep + 1, nextMove)
+                #self.currentState.children.append(GameState(theGame.getGameState(), dep + 1, nextState))
+                if(not self.visited.get(childState, False)):
+                    if(not childState in self.currentState.children):
+                        self.currentState.children.append(childState)
+                        childState.parent = self.currentState
 
-                nextStatid = self.currentState.nextChildToVisit
-                self.currentState.nextChildToVisit += 1
-
-                nextState = self.currentState.children[nextStatid].requiredMovable
-                theGame.makeMove(nextState)
-                self.currentState = self.currentState.children[nextStatid]
-
-                self.solveOneStep()
-
-            else:
-                theGame.reverseMove(self.currentState.requiredMovable)
-                self.currentState = self.currentState.parent
-
-                for c in self.currentState.children:
-                    if self.visited[c] != True:
-                        self.currentState = c
-                        self.solveOneStep()
-        else:
-                theGame.reverseMove(self.currentState.requiredMovable)
-                self.currentState = self.currentState.parent
-
-                for c in self.currentState.children:
-                    if self.visited[c] != True:
-                        self.currentState = c
-                        self.solveOneStep()
-
-
+                theGame.reverseMove(nextMove)
+                #self.currentState.children[i].parent = self.currentState
             
+            MovesLeft = False
+            
+            for kiddos in self.currentState.children:#going down the  branch and becoming a human child or a kiddo and making 
+                nextMove = kiddos.requiredMovable #sure we got visited as an old man by those very childrens
+                theGame.makeMove(nextMove)
+
+                # if(self.visited.get(kiddos, False)):# 
+                #     theGame.reverseMove(nextMove)
+                
+                self.visited[kiddos] = True
+                self.currentState = kiddos
+                MovesLeft = True
+                break
+            
+            if(MovesLeft == False):#if theres no moves/kids left we got to the parent
+                self.currentState = self.currentState.parent
+                self.solveOneStep()
+        else:#if no kids we go to the parent
+            self.currentState = self.currentState.parent
+            self.solveOneStep()
+        
+            
+        if(theGame.getGameState() == victory):#check if we got it else no
+            return True
+        else:
+            return False
+        
+
+            # nextStatid = self.currentState.nextChildToVisit
+            # self.currentState.nextChildToVisit += 1
+
+            # nextState = self.currentState.children[nextStatid].requiredMovable
+            # theGame.makeMove(nextState)
+            # self.currentState = self.currentState.children[nextStatid]
+
+            # self.solveOneStep()
+               
 
         
 class SolverBFS(UninformedSolver):
     def __init__(self, gameMaster, victoryCondition):
         super().__init__(gameMaster, victoryCondition)
+        self.TheQueue = queue.Queue()
 
     def solveOneStep(self):
         """
@@ -97,4 +112,85 @@ class SolverBFS(UninformedSolver):
             True if the desired solution state is reached, False otherwise
         """
         ### Student code goes here
-        return True
+        theGame = self.gm #just the game master not game state
+        victory = self.victoryCondition
+        #checkState = theGame.getGameState()
+        #visted = self.visited[self.currentState]
+
+        # if(checkState == victory):
+        #     return True ##or do nothing
+        
+        if self.TheQueue.empty():#if the queue is empty we have to put something in it else we are gonna pop what was in it
+
+            childrenLen = len(self.gm.getMovables())
+            childs = self.gm.getMovables()
+            dep = self.currentState.depth
+
+            for i in range(0,childrenLen):#just putting all the kids with their parent
+                nextMove = childs[i]
+                theGame.makeMove(nextMove)
+
+                childState = GameState(theGame.getGameState(), dep + 1, nextMove)
+                #self.currentState.children.append(GameState(theGame.getGameState(), dep + 1, nextState))
+                if(not self.visited.get(childState, False)):
+                    if(not childState in self.currentState.children):
+                        self.currentState.children.append(childState)
+                        childState.parent = self.currentState
+                        self.visited[childState] = True
+                        self.TheQueue.put_nowait(childState)
+
+                theGame.reverseMove(nextMove)
+
+        nextState = self.TheQueue.get()
+
+        while 1: #this just gets us to the root node and then stops
+            if not self.currentState.parent:
+                break
+
+            prevStat = self.currentState.requiredMovable
+            theGame.reverseMove(prevStat)
+            self.currentState = self.currentState.parent
+        #now we gotta find where we were in the tree
+        while nextState.depth != self.currentState.depth: #just us going down under as the assuies say
+            theMove = nextState
+
+            while(self.currentState.depth + 1) != theMove.depth:#getting right underneath the current state
+                theMove = theMove.parent
+            
+            for kiddos in self.currentState.children:#then if its the right kid we gonna hop into that
+                if kiddos.requiredMovable == theMove.requiredMovable:
+                    theGame.makeMove(kiddos.requiredMovable)
+                    self.currentState = kiddos
+                    break
+
+        if(theGame.getGameState() == victory):
+            return True
+
+        childrenLen = len(self.gm.getMovables())
+        childs = self.gm.getMovables()
+        dep = self.currentState.depth
+         
+        for i in range(0,childrenLen):#just putting all the kids with their parent
+                nextMove = childs[i]
+                theGame.makeMove(nextMove)
+
+                childState = GameState(theGame.getGameState(), dep + 1, nextMove)
+                #self.currentState.children.append(GameState(theGame.getGameState(), dep + 1, nextState))
+                if(not self.visited.get(childState, False)):
+                    if(not childState in self.currentState.children):
+                        self.currentState.children.append(childState)
+                        childState.parent = self.currentState
+                        self.visited[childState] = True
+                        self.TheQueue.put_nowait(childState)
+
+                theGame.reverseMove(nextMove)
+
+
+
+
+
+
+
+        
+
+        
